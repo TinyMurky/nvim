6afb831f602ac8ea0d10031953476e7a03d57789
TinyMurky <murky0830@gmail.com>
2024-12-14 23:16:16 +0800

    change copy shortcut

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,8 +10,8 @@ vim.keymap.set('i', 'kj', '<esc>', { desc = "Escape" })
 -- use +p for paste
 -- +y for
 vim.cmd("set clipboard+=unnamedplus")
-vim.keymap.set('v', '<C-c>', '"+y', { desc = "Copy from clipboard" })
-vim.keymap.set('n', '<C-v>', '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = "Copy from clipboard" })
+vim.keymap.set('n', '<leader>p', '"+p', { desc = "Paste from clipboard" })
 
 -- Tab setting
 -- code inside vim.cmd to change setting of neovim
