1cece54566a8214c41c689374289ae2f6ebd2f60
Tiny_Murky <murky0830@gmail.com>
2024-12-16 18:12:13 +0800

    change lua-line to relative path

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -5,6 +5,14 @@ return {
         require('lualine').setup({
             options = {
                 theme = 'auto',
+            },
+            sections = {
+                lualine_c = {
+                    {
+                      'filename',
+                      path = 1 -- 設置為 1 顯示相對路徑，設置為 3 顯示完整路徑
+                    }
+                  }
             }
         })
     end
